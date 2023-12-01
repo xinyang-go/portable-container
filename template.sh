dc7fedb3ded205df31c7fa91e797b71885c3124f
xinyang <41963211+xinyang-go@users.noreply.github.com>
2023-12-01 19:23:19 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -11,9 +11,9 @@ mount -t tmpfs -o size=128M tmpfs $dir
 mkdir -p $dir/squashfs $dir/overlay/upper $dir/overlay/work $dir/rootfs
 mount -o offset=$offset $0 $dir/squashfs
 mount -t overlay overlay -o lowerdir=$dir/squashfs,upperdir=$dir/overlay/upper,workdir=$dir/overlay/work $dir/rootfs
-mount -o bind /dev $dir/rootfs/dev
 mount -t proc /proc $dir/rootfs/proc
 mount -t sysfs /sys $dir/rootfs/sys
+mount -o rbind /dev $dir/rootfs/dev
 mount -o bind,ro /etc/resolv.conf $dir/rootfs/etc/resolv.conf
 chroot $dir/rootfs /bin/bash $@
 "
